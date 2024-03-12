061bd1ea8a39db5942d6d3d1500735c9d9cc2b47
Test-Github-explore <158149339+Test-Github-explore@users.noreply.github.com>
2024-03-12 18:07:22 +0900

    Update MEM30-C-1.c

--- MEM30-C-1.c
+++ MEM30-C-1.c
@@ -10,5 +10,14 @@ void free_list(struct node *head) {
   for (struct node *p = head; p != NULL; p = p->next) {
     free(p);
   }
-  return;
+}
+
+int main(void) {
+  struct node *head = malloc(sizeof(struct node));
+  if (head == NULL) {
+    /* Handle error */
+  }
+  head->next = NULL;
+  free_list(head);
+  return 0;
 }
